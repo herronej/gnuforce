7cf604b750a5e013849b7dc38789d593e4b5bdb9
Matthew Schnider <schnider_ms@hammer.cs.mercer.edu>
2018-04-26 14:35:21 -0400

    jacobi working

--- fortran/serial_diffusion.f90
+++ fortran/serial_diffusion.f90
@@ -1,7 +1,7 @@
 program mpi_diffusion
 use cube_mem
 implicit none
-real(kind=8) :: deltas, lRoom, tStep, dcoef, tAccum=0,cMax,cMin
+real(kind=8) :: h,hsqrd, lRoom, tStep, dcoef, tAccum,cMax,cMin,urms
 integer :: rank, size, ierr, i, j, k, t0=0, t1=1,it
 integer, parameter :: N=10
 logical :: partition
@@ -27,16 +27,18 @@ logical :: partition
         !    enddo
         !        print *,""
         !enddo
-    lRoom = 5
-    tStep = (lRoom/250.d0)/dble(N)
-    deltas = lRoom/dble(N)
-    deltas = deltas**2.d0
+    lRoom = 5.d0
+    urms = 250.d0
+    tStep = (lRoom/urms)/dble(N)
+    h = lRoom/dble(N)
+    hsqrd = h**2.d0
 
     cube(1,1,1) = 1e21
-    dcoef = 0.175 * tstep / deltas
+    dcoef = (0.175d0 * tstep) / hsqrd
     cMax = cube(1,1,1)
     cMin = 0
-    do while(cMin/Cmax <= 0.99)
+    tAccum = 0
+    do while(cMin/cMax <= 0.99)
         tAccum = tAccum + tStep
         do i = 1, N
             do j = 1, N
@@ -48,21 +50,23 @@ logical :: partition
                     cube(j,i+1,k)*mask(j,i+1,k) + cube(j,i-1,k)*mask(j,i-1,k) + &
                     cube(j,i,k+1)*mask(j,i,k+1) + cube(j,i,k-1)*mask(j,i,k-1) - &
                     dble(mask(j+1,i,k)+mask(j-1,i,k)+mask(j,i-1,k)+mask(j,i+1,k) +&
-                        mask(j,i,k-1)+mask(j,i,k+1))*(cube(j,i,k))) * dcoef/deltas
+                        mask(j,i,k-1)+mask(j,i,k+1))*(cube(j,i,k))) * dcoef / h
                 enddo
             enddo
+        enddo
             cMax = maxval(cubeCopy)
             cMin = minval(cubeCopy)
-        enddo
+            print *, cMin,  cMax   ,cMin/cMax
         do i = 1, N
             do j = 1, N
                 do k = 1 ,N
                     cube(j,i,k) = cubeCopy(j,i,k)
-                !    print *, cube(j,i,k)
+            !        print *, cube(j,i,k)
                 enddo
             enddo
         enddo
     enddo
+    print *, tStep
     print *, sum(cube)
     print *, tAccum
     deallocate(cube)
