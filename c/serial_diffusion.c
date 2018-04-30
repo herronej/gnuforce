f9a849c1bbb24f24dea589ac3592b1363d88a904
Emily Herron <emily.joyce.herron@live.mercer.edu>
2018-04-30 17:35:29 -0400

    current version of my diffusion

--- c/serial_diffusion.c
+++ c/serial_diffusion.c
@@ -6,67 +6,151 @@
 #define N 10
 
 int main(){
-	double deltas, lRoom, tStep, dcoef;
+	double h, hsqrd, lRoom, tStep, dcoef, tAccum, cMax, cMin, urms;
 	int rank, size, ierr, i, j, k, t0=0, t1=1, it;
 	int partition;
 	
-	double *cube = malloc((N+1)*(N+1)*(N+1)*sizeof(double));
-    	double *cubeCopy = malloc(N*N*N*sizeof(double));
-    	lRoom = 5;
-    	tStep = (lRoom/250.0)/(double)N;
-    	deltas = lRoom/(double)N;
-    	deltas = pow(deltas,2.0);
+	double *cube = malloc((N+2)*(N+2)*(N+2)*sizeof(double));
+	double *mask = malloc((N+2)*(N+2)*(N+2)*sizeof(double));
+    	double *cubeCopy = malloc((N)*(N)*(N)*sizeof(double));
+
+	for(int i = 1; i <= N; i++){
+                for(int j = 1; j <= N; j++){
+                        for(int k =1; k <= N; k++){
+                                mask[i,j,k] = 1.0;//mask[i*N*N+j*N+k] = 1.0;
+                        }
+                }
+        }
+
+
+
+    	lRoom = 5.0;
+	urms = 250.0;
+    	tStep = (lRoom/urms)/(double)N;
+	h = lRoom/(double)N;
+    	hsqrd = pow(h,2.0);
 
     	//cube[0] = 1e21;
-    	dcoef = 0.175 * tStep / deltas;
+    	/*dcoef = 0.175 * tStep / hsqrd;
+	cMax = cube[1*N*N+1*N+1];
+	cMin = 0;
+	tAccum = 0;*/
 
-	for(int i = 0; i < N+1; i++){
-                for(int j = 0; j < N+1; j++){
-                        for(int k =0; k < N+1; k++){
-				cube[i*N*N+j*N+k] = 0.0;
+	for(int i = 0; i < N+2; i++){
+                for(int j = 0; j < N+2; j++){
+                        for(int k =0; k < N+2; k++){
+				cube[i,j,k] = 0.0;//cube[i*N*N+j*N+k] = 0.0;
 			}
 		}
 	}
-	cube[N*N+N+1] = 1e21;
+	cube[1*N*N+1*N+1] = 1e21;
+	dcoef = 0.175 * tStep / hsqrd;
+        cMax = cube[1,1,1];
+        cMin = 0;
+        tAccum = 0;
 
+    	while(cMin/cMax <= 0.99){
 
-    	for(int it =1; it < 1000; it++){
-
+		tAccum = tAccum + tStep;
 		//printf("%f\n", cube[0]);
-
-        	for(int i = 1; i < N; i++){
-            		for(int j = 1; j < N; j++){
-                		for(int k =1; k < N; k++){
+		
+        	for(int i = 1; i <= N; i++){
+            		for(int j = 1; j <= N; j++){
+                		for(int k =1; k <= N; k++){
 
 					//printf("%d %d %d\n", i, j, k);
+					//double c_current_cell = C[i][j][k];
+                    			/*cubeCopy[(i-1)*N*N+(j-1)*N+(k-1)] = cube[i*N*N+j*N+k] + (
+                    			cube[(i+1)*N*N+j*N+k]*mask[(i+1)*N*N+j*N+k] + cube[(i-1)*N*N+j*N+k]*mask[(i-1)*N*N+j*N+k] +
+                    			cube[i*N*N+(j+1)*N+k]*mask[i*N*N+(j+1)*N+k] + cube[i*N*N+(j-1)*N+k]*mask[i*N*N+(j-1)*N+k] +
+                    			cube[i*N*N+j*N+(k+1)]*mask[i*N*N+j*N+(k+1)] + cube[i*N*N+j*N+(k-1)]*mask[i*N*N+j*N+(k-1)] -
+                    			(double)(mask[(i+1)*N*N+j*N+k]+mask[(i-1)*N*N+j*N+k]+mask[i*N*N+(j+1)*N+k]+mask[i*N*N+(j-1)*N+k]+
+						mask[i*N*N+j*N+(k+1)]+mask[i*N*N+j*N+(k-1)])*cube[i*N*N+j*N+k])*2*dcoef;
+					*/
 
-                    			cubeCopy[i*N*N+j*N+k] = cube[i*N*N+j*N+k] + (
-                    			cube[(i+1)*N*N+j*N+k] + cube[(i-1)*N*N+j*N+k] +
-                    			cube[i*N*N+(j+1)*N+k] + cube[i*N*N+(j-1)*N+k] +
-                    			cube[i*N*N+j*N+(k+1)] + cube[i*N*N+j*N+(k-1)] -
-                    			6.0*(cube[i*N*N+j*N+k])) * dcoef / deltas;
+					cubeCopy[i-1,j-1,k-1] = cube[i,j,k] + (
+                                        cube[(i+1),j,k]*mask[(i+1),j,k] + cube[(i-1),j,k]*mask[(i-1),j,k] +
+                                        cube[i,(j+1),k]*mask[i,(j+1),k] + cube[i,(j-1),k]*mask[i,(j-1),k] +
+                                        cube[i,j,(k+1)]*mask[i,j,(k+1)] + cube[i,j,(k-1)]*mask[i,j,(k-1)] -
+                                        (double)(mask[(i+1),j,k]+mask[(i-1),j,k]+mask[i,(j+1),k]+mask[i,(j-1),k]+
+                                                mask[i,j,(k+1)]+mask[i,j,(k-1)])*cube[i,j,k])*2*dcoef;
+
+					
+					/*if(cMin > c_current_cell){
+		                            cMin = c_current_cell;
+                		            mini = i;
+                            		    minj = j;
+                         		    mink = k;   
+                        		}
+                        		if(c_max < c_current_cell)
+                            			cMax = c_current_cell;*/
                 		}
             		}
         	}
-		for(int i = 1; i < N; i++){
-                        for(int j = 1; j < N; j++){
-                                for(int k = 1; k < N; k++){
-                    			cube[i*N*N+j*N+k] = cubeCopy[i*N*N+j*N+k];
-                    			printf("%f\n", cube[i*N*N+j*N+k]);
+		
+		/*for (i=0; i<N; i++) {
+	                for (j=0; j<N; j++){
+        	                for (k=0; k<N; k++){
+					if (cubeCopy[i*N*N+j*N+k] < cMin) {
+                        	                cMin = cubeCopy[i*N*N+j*N+k];
+                                	}
+                                	if (cubeCopy[i*N*N+j*N+k] > cMax) {
+                                        	cMax = cubeCopy[i*N*N+j*N+k];
+                                	}	
+				}
+			}
+		}*/
+		
+		cMax = cubeCopy[0];
+		for (i=1;i<(N)*(N)*(N);i++)
+			if(cubeCopy[i] > cMax)
+				cMax = cubeCopy[i];
+		cMin = cubeCopy[0];
+		for (i=1;i<(N)*(N)*(N);i++)
+                        if(cubeCopy[i] < cMin)
+                                cMin = cubeCopy[i];
+
+
+		//printf("%f\n", cMin);
+		//printf("%f\n", cMax);
+		//printf("%f\n", cMin/cMax);
+
+
+		
+		for(int i = 1; i <= N; i++){
+                        for(int j = 1; j <= N; j++){
+                                for(int k = 1; k <= N; k++){
+                    			cube[i,j,k] = cubeCopy[(i-1),(j-1),(k-1)];
+                    			//printf("%f\n", cube[i*N*N+j*N+k]);
                 		}
             		}
         	}
+
+
+		double tot = 0;
+	        for (i=0; i<N+2; i++) {
+        	        for (j=0; j<N+2; j++){
+                	        for (k=0; k<N+2; k++){
+                        	        tot = tot + cube[i,j,k];
+                        	}
+                	}
+        	}
+		printf("%f\n",tot);
+
     	}
 	
 	double tot = 0;
-	for (i=0; i<N+1; i++) {
-                for (j=0; j<N+1; j++){
-                        for (k=0; k<N+1; k++){
-                                tot = tot + cube[i*N*N+j*N+k];
+	for (i=0; i<N+2; i++) {
+                for (j=0; j<N+2; j++){
+                        for (k=0; k<N+2; k++){
+                                tot = tot + cube[i,j,k];
                         }
                 }
         }
 
     	printf("%f\n",tot);
+	printf("%f\n",tAccum);
     	free(cube);
+	free(cubeCopy);
+	free(mask);
 }
